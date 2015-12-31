dc08fa6585a2d7786f9fd0ef87c9e6e58124f6db
Veerabahu Subramanian C <moonpearl_vegtime_2000@yahoo.com>
2015-12-31 13:53:33 +0530

    Adding view transaction as a fragment

--- FinanceApp/app/src/main/java/com/veechand/financeapp/ActionChooserActivity.java
+++ FinanceApp/app/src/main/java/com/veechand/financeapp/ActionChooserActivity.java
@@ -1,42 +1,130 @@
 package com.veechand.financeapp;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class ActionChooserActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button viewTransaction;
-    Button addTransaction;
+    private Button viewTransaction;
+    private Button addTransaction;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String mActivityTitle;
+    private String[] actions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action_chooser);
 
+        /*
         viewTransaction = (Button) findViewById(R.id.viewTransactionButton);
         addTransaction = (Button) findViewById(R.id.addTransactionButton);
 
         viewTransaction.setOnClickListener(this);
         addTransaction.setOnClickListener(this);
+        */
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        actions = getResources().getStringArray(R.array.actions);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, actions));
+        mActivityTitle = getTitle().toString();
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                displayView(position);
+            }
+        });
+        setupDrawer();
+        displayView(1); //chosing the view transaction fragment on start
+    }
+
+    private void displayView(int position){
+        Fragment fragment = null;
+        switch (position) {
+            case 0:
+                Log.i("Cases ","Case 0");
+                break;
+            case 1:
+                Log.i("Cases","Case 1");
+                fragment = new ViewTransactionFragment();
+                break;
+            case 2:
+                Log.i("Cases","Case 2");
+                break;
+        }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame, fragment).commit();
+            mDrawerList.setItemChecked(position, true);
+            mDrawerList.setSelection(position);
+            Log.i("Title set is ",actions[position]);
+            getSupportActionBar().setTitle(actions[position]);
+            mDrawerLayout.closeDrawer(mDrawerList);
+        } else {
+            // error in creating fragment
+            Log.e("MainActivity", "Error in creating fragment");
+        }
+
+    }
+    private void setupDrawer() {
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Navigation!");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+               // getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerToggle.setHomeAsUpIndicator(R.drawable.ic_drawer);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        /*switch (v.getId()){
             case R.id.viewTransactionButton:
-                Intent viewTransaction = new Intent(ActionChooserActivity.this,ViewTransactionActivity.class);
+                Intent viewTransaction = new Intent(ActionChooserActivity.this,ViewTransactionFragment.class);
                 startActivity(viewTransaction);
                 break;
             case R.id.addTransactionButton:
                 Intent addTransactionIntent = new Intent(ActionChooserActivity.this,AddTransactionActivity.class);
                 startActivity(addTransactionIntent);
                 break;
-        }
+        }*/
     }
 
     @Override
@@ -48,6 +136,9 @@ public class ActionChooserActivity extends AppCompatActivity implements View.OnC
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
         switch (item.getItemId()){
             case R.id.menu_transaction_subtype_add:
                 Intent intent = new Intent(ActionChooserActivity.this, TransactionSubTypeAdd.class);
@@ -56,7 +147,7 @@ public class ActionChooserActivity extends AppCompatActivity implements View.OnC
             default:
                 return super.onOptionsItemSelected(item);
         }
+   }
 
 
-    }
 }
---
+++ FinanceApp/app/src/main/java/com/veechand/financeapp/ViewTransactionFragment.java
@@ -0,0 +1,65 @@
+package com.veechand.financeapp;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.veechand.financeapp.db.DBHelper;
+
+import java.util.List;
+
+public class ViewTransactionFragment extends Fragment {
+
+    private RecyclerView transactionRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.Adapter mAdapter;
+    private DBHelper dbHelper;
+
+    public ViewTransactionFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.i("Cases","CAlled onCreateView");
+        View view =  inflater.inflate(R.layout.activity_view_transaction, container, false);
+        this.transactionRecyclerView = (RecyclerView) view.findViewById(R.id.transaction_recycler_view);
+        transactionRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        transactionRecyclerView.setLayoutManager(mLayoutManager);
+        this.dbHelper = new DBHelper(this.getActivity());
+        List myDataset = dbHelper.getAllTransactions();
+        mAdapter = new TransactionAdapter(myDataset);
+        transactionRecyclerView.setAdapter(mAdapter);
+        return  view;
+    }
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    /*    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_transaction);
+        this.transactionRecyclerView = (RecyclerView) findViewById(R.id.transaction_recycler_view);
+        transactionRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        transactionRecyclerView.setLayoutManager(mLayoutManager);
+        this.dbHelper = new DBHelper(ViewTransactionFragment.this);
+        List myDataset = dbHelper.getAllTransactions();
+        mAdapter = new TransactionAdapter(myDataset);
+        transactionRecyclerView.setAdapter(mAdapter);
+    }*/
+
+}
